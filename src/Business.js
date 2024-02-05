74729ec30a0d291a3cb9301b0c26778595eb3add
cboise@swbno.org <cboise@swbno.org>
2024-02-05 10:50:30 -0600

    Create Business Component

---
+++ src/Business.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Business() {
+    return (
+        <div>
+            <img className='image'/>
+            <p className='name'>name: </p>
+            <p className='address'>address: </p>
+            <p className='city'>city: </p>
+            <p className='state'>state: </p>
+            <p className='zipcode'>zipcode: </p>
+            <p className='category'>category: </p>
+            <p className='rating'>rating: </p>
+            <p className='reviews'>review count: </p>
+      </div>
+
+        </div>
+
+    );
+}
+
+export default Business;
